425300678a123a5ea4d522cd0c9563286d4327f9
Ubuntu <ubuntu@ip-172-31-25-162.us-west-2.compute.internal>
2013-07-16 23:30:10 +0000

    updated web.js

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ app.get('/', function(request, response) {
   var fs = require('fs');
   var buffer = new Buffer(256);
 
-  var data = fs.readFileSync('index.html', 'encoding');
+  var data = fs.readFileSync('index.html', 'utf8');
   var d = buffer.write(data);
   var res = buffer.toString(d);
   response(res);
